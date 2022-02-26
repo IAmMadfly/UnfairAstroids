f985f44a81e613dcfc1523cfac81af4372665e4a
Karl Runge <karl.spartacus@gmail.com>
2022-02-26 21:51:36 +1100

    First working sprite

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::SpritePlugin};
 
 fn main() {
     let mut app = App::new();
 
     app.add_plugins(DefaultPlugins);
+    app.add_plugin(SpritePlugin::default());
 
     app.add_startup_system(load_camera.system());
     app.add_startup_system(load_players.system());
 
     app.add_system(generate_astroids.system());
-    app.add_system(printouts.system());
+    // app.add_system(printouts.system());
 
     app.run();
 }
@@ -17,12 +18,12 @@ fn main() {
 fn load_camera(mut commands: Commands) {
     println!("Loading the camera!");
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(UiCameraBundle::default());
+    // commands.spawn_bundle(UiCameraBundle::default());
 }
 
 fn load_players(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("assets/sprites/player_ships/ship_0000.png"),
+        texture: asset_server.load("sprites/player_ships/ship_0000.png"),
         ..Default::default()
     });
 }
