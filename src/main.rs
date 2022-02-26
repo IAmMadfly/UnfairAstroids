a3a310a960abd72efb51efea716191307c989ec3
Karl Runge <karl.spartacus@gmail.com>
2022-02-26 21:34:37 +1100

    Changes

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,28 @@ fn main() {
     app.add_startup_system(load_players.system());
 
     app.add_system(generate_astroids.system());
+    app.add_system(printouts.system());
 
     app.run();
 }
 
 fn load_camera(mut commands: Commands) {
+    println!("Loading the camera!");
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
 }
 
-fn load_players(mut commands: Commands) {
+fn load_players(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(1.0, 0.0, 0.0),
-            flip_x: false,
-            flip_y: false,
-            custom_size: Some(Vec2::new(200.0, 200.0)),
-        },
-        transform: Transform::from_xyz(100.0, 100.0, 0.1),
-        global_transform: Default::default(),
-        texture: Default::default(),
-        visibility: Visibility { is_visible: true },
+        texture: asset_server.load("assets/sprites/player_ships/ship_0000.png"),
+        ..Default::default()
     });
 }
 
 fn generate_astroids() {}
+
+fn printouts(player: Query<&Transform, With<Sprite>>) {
+    for player in player.iter() {
+        println!("Something is here: {:?}", player);
+    }
+}
